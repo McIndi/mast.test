262f0f6fa0de317a2b63d4000c89f764440afd0c
ilovetux <me@ilovetux.com>
2016-01-22 12:00:20 -0500

    BUG: Fix package name in mast.test

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "mast.cli",
+    name = "mast.test",
     version = "2.1.0",
     author = "Clifford Bressette",
     author_email = "cliffordbressette@mcindi.com",

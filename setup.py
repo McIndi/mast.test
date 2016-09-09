6ec34d60ce4ef98db4f97ad3b86e21209e486596
ilovetux <me@ilovetux.com>
2016-09-09 13:52:59 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name = "mast.test",
-    version = "2.1.0",
+    version = "2.2.0",
     author = "Clifford Bressette",
     author_email = "cliffordbressette@mcindi.com",
     description = ("A simple XML based integration testing framework"),
